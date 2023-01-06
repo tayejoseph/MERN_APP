97461365ff07747a34de46e6dc6e74f8e74e5182
Taye Joseph <tayejoseph6@gmail.com>
2023-01-06 22:39:46 +0100

    implemented CRUD operations for goals

--- backend/controllers/goalControler.js
+++ backend/controllers/goalControler.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @des  Get goals
 // @route GET /api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @des  set goals
@@ -13,21 +15,49 @@ const getGoals = asyncHandler(async (req, res) => {
 // @access Private
 
 const setGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Set goals" });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // @des  Update goals
 // @route PUT /api/goals
 // @access Private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  // this findById and updates it else if it does not exists it created a new one
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @des  Delete goals
 // @route DELETE /api/goals
 // @access Private
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
